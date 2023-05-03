628446c07f6ba2dc27ea3695c30657c55c54680f
RahulAnup <rahulanup03@gmail.com>
2023-05-03 23:15:47 +0530

    initial commit

---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+function Navbar() {
+  return (
+    <div className='nav'>
+        <h2 className="nav__logo">filmflix</h2>
+        <h3 className="nav__sign-in">Sign-in</h3>
+    </div>
+  )
+}
+
+export default Navbar
